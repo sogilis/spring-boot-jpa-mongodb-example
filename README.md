8a8f43725c5d2c3159714f116210462c1d74972e
Jibidus <jibidus@gmail.com>
2017-06-20 15:47:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spring boot + JPA + MongoDB application sample
+
+This application demonstrate how to build a Spring Boot application which can switch between JPA and MongoDB with only configuration changes.
+
+
+# How to run application
+
+```bash
+./gradlew bootRun
+```
---
+++ src/main/java/com/sogilis/example/springbootjpamongo/SpringBootJpaMongoApplication.java
@@ -0,0 +1,12 @@
+package com.sogilis.example.springbootjpamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaMongoApplication.class, args);
+	}
+}
