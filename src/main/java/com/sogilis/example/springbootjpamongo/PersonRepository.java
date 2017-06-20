8081f12a1da11abf0fe448d9541f9e38b09ca960
Jibidus <jibidus@gmail.com>
2017-06-20 19:07:36 +0200

    Make repository extends PagingAndSortingRepository (instead of CrudRepository)

--- src/main/java/com/sogilis/example/springbootjpamongo/PersonRepository.java
+++ src/main/java/com/sogilis/example/springbootjpamongo/PersonRepository.java
@@ -1,8 +1,8 @@
 package com.sogilis.example.springbootjpamongo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, String> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, String> {
 }
