005e9c05c456a229bb1e43148056ed58d6958e4b
Jibidus <jibidus@gmail.com>
2017-06-21 13:47:00 +0200

    Remove Auditing

--- src/main/java/com/sogilis/example/springbootjpamongo/JpaConfiguration.java
+++ src/main/java/com/sogilis/example/springbootjpamongo/JpaConfiguration.java
@@ -2,12 +2,10 @@ package com.sogilis.example.springbootjpamongo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories("com.sogilis.example.springbootjpamongo")
-@EnableJpaAuditing
 @Profile("jpa")
 public class JpaConfiguration {
 }
--- src/main/java/com/sogilis/example/springbootjpamongo/MongoConfiguration.java
+++ src/main/java/com/sogilis/example/springbootjpamongo/MongoConfiguration.java
@@ -2,12 +2,10 @@ package com.sogilis.example.springbootjpamongo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories("com.sogilis.example.springbootjpamongo")
-@EnableMongoAuditing
 @Profile("mongodb")
 public class MongoConfiguration {
 }
