b4493a97203db545a0474807edd96063757bd20a
Jibidus <jibidus@gmail.com>
2017-06-21 13:56:50 +0200

    Remove unnecessary annotation on @Configuration

--- src/main/java/com/sogilis/example/springbootjpamongo/JpaConfiguration.java
+++ src/main/java/com/sogilis/example/springbootjpamongo/JpaConfiguration.java
@@ -2,10 +2,11 @@ package com.sogilis.example.springbootjpamongo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories("com.sogilis.example.springbootjpamongo")
 @Profile("jpa")
+// Custom JPA configuration here
+// @EnableJpaRepositories("com.sogilis.example.springbootjpamongo")
+// @EnableJpaAuditing
 public class JpaConfiguration {
 }
--- src/main/java/com/sogilis/example/springbootjpamongo/MongoConfiguration.java
+++ src/main/java/com/sogilis/example/springbootjpamongo/MongoConfiguration.java
@@ -2,10 +2,11 @@ package com.sogilis.example.springbootjpamongo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories("com.sogilis.example.springbootjpamongo")
 @Profile("mongodb")
+// Custom Mongo configuration here
+// @EnableMongoRepositories("com.sogilis.example.springbootjpamongo")
+// @EnableMongoAuditing
 public class MongoConfiguration {
 }
